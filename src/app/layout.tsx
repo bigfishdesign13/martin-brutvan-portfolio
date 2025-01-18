ca28664bd7963a00524cf02789a69387364f0747
bigfishdesign13 <marty@brutvan.com>
2025-01-18 10:29:14 -0500

    added basic GA tracking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
-import "./globals.css";
+import Script from "next/script";
 import Header from "../components/Header";
 import { Passion_One, Roboto_Slab } from "next/font/google";
+import "./globals.css";
 
 const robotoSlab = Roboto_Slab({
   subsets: ["latin"],
@@ -25,6 +26,8 @@ export const metadata: Metadata = {
   },
 };
 
+const gaId = "G-20ZXRJQ3B6";
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -35,6 +38,24 @@ export default function RootLayout({
       <body
         className={`${passionOne.variable} ${robotoSlab.variable} bg-light-surface-primary antialiased`}
       >
+        <Script
+          strategy="afterInteractive"
+          src={`https://www.googletagmanager.com/gtag/js?id=${gaId}`}
+        />
+        <Script
+          id="gtag-init"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gaId}', {
+              page_path: window.location.pathname,
+            });
+          `,
+          }}
+        />
         <Header />
         <main className="flex flex-col w-full max-w-screen-lg px-4 py-20 mt-16">
           {children}
