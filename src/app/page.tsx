ccd97767897a797c2826fe07b9354de58dd6f182
bigfishdesign13 <marty@brutvan.com>
2025-01-20 21:39:41 -0500

    undo pt 1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,13 +22,9 @@ export default function Home() {
           </Heading>
           <p className="flex flex-col md:flex-row items-center mt-2 md:mt-0 text-light-heading-secondary text-[22px] font-bold">
             <span>designer</span>
-            <span className="mx-2 text-light-border-primary hidden md:inline">
-              &bull;
-            </span>
+            <span className="mx-2 text-light-border-primary hidden md:inline">&bull;</span>
             <span>engineer</span>
-            <span className="mx-2 text-light-border-primary hidden md:inline">
-              &bull;
-            </span>
+            <span className="mx-2 text-light-border-primary hidden md:inline">&bull;</span>
             <span>communicator</span>
           </p>
         </div>
