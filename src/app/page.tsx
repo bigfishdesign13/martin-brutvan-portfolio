b2e1119ad4f75d84219a8197781fbfe1261a91cd
bigfishdesign13 <marty@brutvan.com>
2025-01-20 21:41:09 -0500

    redo v2 pt 1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
 import Heading from "../components/Heading";
+// import Image from "next/image";
 
 export default function Home() {
   return (
     <>
-      <div className="flex flex-col h-[70vh] justify-center">
+      <div className="flex flex-col h-[90%]">
         <div className="flex items-center md:items-start flex-col md:ps-16 py-[4rem] md:border-s-[12px] md:border-light-border-primary md:w-min">
           <Heading
             level="h1"
             size="display1"
+            // className="font-heading base:text-base sm:text-sm md:text-md lg:text-lg xl:text-xl text-light-heading-primary"
             className="text-light-heading-primary leading-[.72] text-center md:text-left"
           >
             Martin Brutvan
@@ -22,9 +24,13 @@ export default function Home() {
           </Heading>
           <p className="flex flex-col md:flex-row items-center mt-2 md:mt-0 text-light-heading-secondary text-[22px] font-bold">
             <span>designer</span>
-            <span className="mx-2 text-light-border-primary hidden md:inline">&bull;</span>
+            <span className="mx-2 text-light-border-primary hidden md:inline">
+              &bull;
+            </span>
             <span>engineer</span>
-            <span className="mx-2 text-light-border-primary hidden md:inline">&bull;</span>
+            <span className="mx-2 text-light-border-primary hidden md:inline">
+              &bull;
+            </span>
             <span>communicator</span>
           </p>
         </div>
