fabdd4283c0bd7a783cea88e910ffeafa0cf5463
bigfishdesign13 <marty@brutvan.com>
2025-12-09 20:02:23 -0500

    added github link in main menu

--- src/components/SocialLinks.tsx
+++ src/components/SocialLinks.tsx
@@ -9,13 +9,28 @@ const SocialLinks = () => {
       <Link
         aria-label="Visit my LinkedIn profile"
         className={styles.link}
-        href="https://www.linkedin.com/in/martybrutvan/"
+        href="https://linkedin.com/in/martybrutvan/"
         target="_blank"
       >
         <Image
           alt="LinkedIn icon"
           aria-hidden
           src="/images/icon-linkedin.svg"
+          className={styles.icon}
+          height={16}
+          width={16}
+        />
+      </Link>
+      <Link
+        aria-label="Visit my GitHub profile"
+        className={styles.link}
+        href="https://github.com/bigfishdesign13"
+        target="_blank"
+      >
+        <Image
+          alt="GitHub icon"
+          aria-hidden
+          src="/images/icon-github.svg"
           className={styles.icon}
           height={16}
           width={16}
